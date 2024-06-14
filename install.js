e52c1433f7193c90c5f49caa8a1d5e6faaf864e9
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-06-14 15:01:32 +0200

    Correction of SQL.

--- install.js
+++ install.js
@@ -15,15 +15,17 @@ const connect = mysql.createConnection ({
 });
 
 //Drop and create table
-connect.query(`DROP IF EXISTS jobs;
-    CREATE TABLE jobs(
+connect.query("DROP TABLE IF EXISTS jobs;");
+
+connect.query(`CREATE TABLE jobs(
         id INT(2) NOT NULL AUTO_INCREMENT,
         companyname VARCHAR(32) NOT NULL,
         jobtitle VARCHAR(64) NOT NULL,
         location VARCHAR(32) NOT NULL,
         startdate DATE NOT NULL,
         enddate DATE NOT NULL,
-        description VARCHAR(128) NOT NULL
+        description VARCHAR(128) NOT NULL,
+        PRIMARY KEY(id)
     );`
 );
 
